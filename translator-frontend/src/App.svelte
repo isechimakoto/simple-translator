54a741d5063d0f4fbcc5c5cf153f206dadb9efec
isechimakoto <misechi@callmehappening.com>
2022-02-20 20:17:20 +0900

    Add the sample implementation of front-end

---
+++ translator-frontend/src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const pageName= 'Simple Translator';
+</script>
+
+<main>
+  <h1 class="text-center font-bold text-4xl">{pageName}</h1>
+</main>
---
+++ translator-frontend/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,svelte,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
